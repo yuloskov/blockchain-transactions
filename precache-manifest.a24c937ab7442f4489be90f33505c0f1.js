60aa4e3651476a77dd1e93009acb7dbbb317238c
Yuloskov Artyom <yuloskov.aa@yandex.ru>
2020-10-08 18:13:20 +0300

    Updates

---
+++ precache-manifest.a24c937ab7442f4489be90f33505c0f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84416735a0165df9b8f5d533adc9618c",
+    "url": "/transaction-tracker/index.html"
+  },
+  {
+    "revision": "3ac1dd428a3b365dce1b",
+    "url": "/transaction-tracker/static/js/2.54ce9818.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/transaction-tracker/static/js/2.54ce9818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ec60e0bdcc136db177",
+    "url": "/transaction-tracker/static/js/main.72cfe925.chunk.js"
+  },
+  {
+    "revision": "6e108a3c258e277fb4bd",
+    "url": "/transaction-tracker/static/js/runtime-main.4008a6b7.js"
+  }
+]);
